d34401b92cb1f10a788d637890946db3cc8d26f5
Kevinluca <kevinmiskandar@gmail.com>
2026-04-24 01:39:20 +0300

    gui 2

--- Rental Form.Designer.cs
+++ Rental Form.Designer.cs
@@ -28,34 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtMID = new System.Windows.Forms.TextBox();
-            this.txtUID = new System.Windows.Forms.TextBox();
+            this.txt_title = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtRVal = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtRID = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.btn_showCopies = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // txtMID
+            // txt_title
             // 
-            this.txtMID.Location = new System.Drawing.Point(160, 42);
-            this.txtMID.Margin = new System.Windows.Forms.Padding(4);
-            this.txtMID.Name = "txtMID";
-            this.txtMID.Size = new System.Drawing.Size(261, 22);
-            this.txtMID.TabIndex = 0;
-            this.txtMID.TextChanged += new System.EventHandler(this.txt_title_TextChanged);
-            // 
-            // txtUID
-            // 
-            this.txtUID.Location = new System.Drawing.Point(160, 94);
-            this.txtUID.Margin = new System.Windows.Forms.Padding(4);
-            this.txtUID.Name = "txtUID";
-            this.txtUID.Size = new System.Drawing.Size(261, 22);
-            this.txtUID.TabIndex = 2;
-            this.txtUID.TextChanged += new System.EventHandler(this.txt_memID_TextChanged);
+            this.txt_title.Location = new System.Drawing.Point(160, 42);
+            this.txt_title.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_title.Name = "txt_title";
+            this.txt_title.Size = new System.Drawing.Size(261, 22);
+            this.txt_title.TabIndex = 0;
             // 
             // label1
             // 
@@ -63,68 +47,20 @@
             this.label1.Location = new System.Drawing.Point(36, 50);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.Size = new System.Drawing.Size(61, 16);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Movie ID";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 97);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 16);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "User ID";
-            // 
-            // txtRVal
-            // 
-            this.txtRVal.Location = new System.Drawing.Point(160, 141);
-            this.txtRVal.Margin = new System.Windows.Forms.Padding(4);
-            this.txtRVal.Name = "txtRVal";
-            this.txtRVal.Size = new System.Drawing.Size(261, 22);
-            this.txtRVal.TabIndex = 11;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(36, 147);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 16);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Rating value";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // txtRID
-            // 
-            this.txtRID.Location = new System.Drawing.Point(160, 195);
-            this.txtRID.Margin = new System.Windows.Forms.Padding(4);
-            this.txtRID.Name = "txtRID";
-            this.txtRID.Size = new System.Drawing.Size(261, 22);
-            this.txtRID.TabIndex = 13;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 201);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 16);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "Rating ID";
-            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            this.label1.Text = "Film Title";
             // 
             // btn_showCopies
             // 
-            this.btn_showCopies.Location = new System.Drawing.Point(152, 281);
-            this.btn_showCopies.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_showCopies.Location = new System.Drawing.Point(160, 151);
+            this.btn_showCopies.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_showCopies.Name = "btn_showCopies";
-            this.btn_showCopies.Size = new System.Drawing.Size(208, 44);
-            this.btn_showCopies.TabIndex = 15;
-            this.btn_showCopies.Text = "insert rating";
+            this.btn_showCopies.Size = new System.Drawing.Size(173, 87);
+            this.btn_showCopies.TabIndex = 14;
+            this.btn_showCopies.Text = "Show movies";
             this.btn_showCopies.UseVisualStyleBackColor = true;
-            this.btn_showCopies.Click += new System.EventHandler(this.btn_showCopies_Click_1);
+            this.btn_showCopies.Click += new System.EventHandler(this.btn_showCopies_Click);
             // 
             // Rental_Form
             // 
@@ -132,15 +68,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(505, 415);
             this.Controls.Add(this.btn_showCopies);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtRID);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.txtRVal);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtUID);
-            this.Controls.Add(this.txtMID);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Controls.Add(this.txt_title);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Rental_Form";
             this.Text = "Rental Form";
             this.Load += new System.EventHandler(this.Rental_Form_Load);
@@ -151,14 +81,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtMID;
-        private System.Windows.Forms.TextBox txtUID;
+        private System.Windows.Forms.TextBox txt_title;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtRVal;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtRID;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_showCopies;
     }
 }
