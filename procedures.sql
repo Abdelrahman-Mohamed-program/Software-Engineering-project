97c7a8814aa7ac458e2cf9982159d43f65f3d267
Abdelrahman Mohamed <146343312+Abdelrahman-Mohamed-program@users.noreply.github.com>
2026-04-28 02:22:35 +0300

    ..

--- procedures.sql
+++ procedures.sql
@@ -1,139 +1,101 @@
-
--- PROCEDURE 1: Search Movies (MULTIPLE ROWS)
--- Used in: Search Form
--- Uses: SysRefCursor (multiple rows)
-
-CREATE OR REPLACE PROCEDURE SearchMovies
-    (keyword IN VARCHAR2, results OUT SYS_REFCURSOR)
+-- PROCEDURE 1: Search Movies
+CREATE OR REPLACE PROCEDURE APP_SearchMovies
+    (p_keyword IN VARCHAR2, p_results OUT SYS_REFCURSOR)
 AS
 BEGIN
-    OPEN results FOR
+    OPEN p_results FOR
         SELECT MovieID, Title, Genre, ReleaseYear, AvgRating
         FROM Movies
-        WHERE UPPER(Title) LIKE UPPER('%' || keyword || '%');
+        WHERE UPPER(Title) LIKE UPPER('%' || p_keyword || '%');
 END;
 /
 
-
-
--- PROCEDURE 2: Get Movie Details (ONE ROW)
--- Used in: Search Form (after selecting a movie)
--- Uses: OUT parameters, NO SysRefCursor
-
-CREATE OR REPLACE PROCEDURE GetMovieDetails
-    (MID       IN  NUMBER,
-     MTitle    OUT VARCHAR2,
-     MGenre    OUT VARCHAR2,
-     MDesc     OUT VARCHAR2,
-     MYear     OUT NUMBER,
-     MRating   OUT NUMBER)
+-- PROCEDURE 2: Get Movie Details
+CREATE OR REPLACE PROCEDURE APP_GetMovieDetails
+    (p_MID       IN  NUMBER,
+     p_Title     OUT VARCHAR2,
+     p_Genre     OUT VARCHAR2,
+     p_Desc      OUT VARCHAR2,
+     p_Year      OUT NUMBER,
+     p_Rating    OUT NUMBER)
 AS
 BEGIN
     SELECT Title, Genre, Description, ReleaseYear, AvgRating
-    INTO MTitle, MGenre, MDesc, MYear, MRating
+    INTO p_Title, p_Genre, p_Desc, p_Year, p_Rating
     FROM Movies
-    WHERE MovieID = MID;
+    WHERE MovieID = p_MID;
 END;
 /
 
-
-
--- PROCEDURE 3: Get Reviews for a Movie (MULTIPLE ROWS)
--- Used in: Search Form (view reviews section)
--- Uses: SysRefCursor (multiple rows)
-
-CREATE OR REPLACE PROCEDURE GetMovieReviews
-    (MID IN NUMBER, ReviewList OUT SYS_REFCURSOR)
+-- PROCEDURE 3: Get Movie Reviews
+CREATE OR REPLACE PROCEDURE APP_GetMovieReviews
+    (p_MID IN NUMBER, p_ReviewList OUT SYS_REFCURSOR)
 AS
 BEGIN
-    OPEN ReviewList FOR
+    OPEN p_ReviewList FOR
         SELECT U.Username, R.ReviewText
-        FROM Reviews R, Users U
-        WHERE R.UserID = U.UserID
-          AND R.MovieID = MID;
+        FROM Reviews R
+        JOIN Users U ON R.UserID = U.UserID
+        WHERE R.MovieID = p_MID;
 END;
 /
 
-
-
--- PROCEDURE 4: Get Max RatingID (ONE ROW / single value)
--- Used in: Rating Form (to generate new RatingID)
--- Uses: OUT parameter, NO SysRefCursor
-
-CREATE OR REPLACE PROCEDURE GetMaxRatingID
-    (RID OUT NUMBER)
+-- PROCEDURE 4: Get Max RatingID
+CREATE OR REPLACE PROCEDURE APP_GetMaxRatingID
+    (p_RID OUT NUMBER)
 AS
 BEGIN
-   SELECT NVL(MAX(RatingID), 0)
-INTO RID
-FROM Ratings;
+    SELECT NVL(MAX(RatingID), 0)
+    INTO p_RID
+    FROM Ratings;
 END;
 /
 
-  
-
--- PROCEDURE 5: Get Max ReviewID (ONE ROW / single value)
--- Used in: Review Form (to generate new ReviewID)
--- Uses: OUT parameter, NO SysRefCursor
-
-CREATE OR REPLACE PROCEDURE GetMaxReviewID
-    (RID OUT NUMBER)
+-- PROCEDURE 5: Get Max ReviewID
+CREATE OR REPLACE PROCEDURE APP_GetMaxReviewID
+    (p_RID OUT NUMBER)
 AS
 BEGIN
     SELECT NVL(MAX(ReviewID), 0)
-    INTO RID
+    INTO p_RID
     FROM Reviews;
 END;
 /
 
-
-
--- PROCEDURE 6: Insert New Rating
--- Used in: Rating Form (Rent button equivalent)
-
-CREATE OR REPLACE PROCEDURE InsertRating
-    (RID NUMBER, UID NUMBER, MID NUMBER, RVal NUMBER)
+-- PROCEDURE 6: Insert Rating
+CREATE OR REPLACE PROCEDURE APP_InsertRating
+    (p_RID NUMBER, p_UID NUMBER, p_MID NUMBER, p_RVal NUMBER)
 AS
 BEGIN
     INSERT INTO Ratings (RatingID, UserID, MovieID, RatingValue)
-    VALUES (RID, UID, MID, RVal);
+    VALUES (p_RID, p_UID, p_MID, p_RVal);
 
-    -- Also update the AvgRating in Movies table
     UPDATE Movies
     SET AvgRating = (
         SELECT AVG(RatingValue)
         FROM Ratings
-        WHERE MovieID = MID
+        WHERE MovieID = p_MID
     )
-    WHERE MovieID = MID;
+    WHERE MovieID = p_MID;
 END;
 /
 
-
-
--- PROCEDURE 7: Insert New Review
--- Used in: Review Form
-
-CREATE OR REPLACE PROCEDURE InsertReview
-    (RID NUMBER, UID NUMBER, MID NUMBER, RTxt VARCHAR2)
+-- PROCEDURE 7: Insert Review
+CREATE OR REPLACE PROCEDURE APP_InsertReview
+    (p_RID NUMBER, p_UID NUMBER, p_MID NUMBER, p_Text VARCHAR2)
 AS
 BEGIN
     INSERT INTO Reviews (ReviewID, UserID, MovieID, ReviewText)
-    VALUES (RID, UID, MID, RTxt);
+    VALUES (p_RID, p_UID, p_MID, p_Text);
 END;
 /
 
-
-
--- PROCEDURE 8: Get All Movies (MULTIPLE ROWS)
--- Used in: both forms to load movie lists
--- Uses: SysRefCursor
-
-CREATE OR REPLACE PROCEDURE GetAllMovies
-    (MovieList OUT SYS_REFCURSOR)
+-- PROCEDURE 8: Get All Movies
+CREATE OR REPLACE PROCEDURE APP_GetAllMovies
+    (p_MovieList OUT SYS_REFCURSOR)
 AS
 BEGIN
-    OPEN MovieList FOR
+    OPEN p_MovieList FOR
         SELECT MovieID, Title, Genre, ReleaseYear, AvgRating
         FROM Movies
         ORDER BY AvgRating DESC;
